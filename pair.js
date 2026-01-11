24391df34a95ebcdf0f7a24e9f075920c417bc51
Zuwa0745 <Zuwaentertaiment@gmail.com>
2026-01-11 03:43:35 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,17 @@
 const { default: makeWASocket, useMultiFileAuthState, delay } = require("@whiskeysockets/baileys");
 const pino = require("pino");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 10000;
+
+// Hii sehemu ndiyo itafanya link yako ifunguke kwenye browser
+app.get('/', (req, res) => {
+    res.send("ZUWA-MD PAIRING SITE IS LIVE!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 async function startPair() {
     const { state, saveCreds } = await useMultiFileAuthState('./session');
@@ -15,7 +27,6 @@ async function startPair() {
         if (connection === "open") {
             await delay(5000);
             let session_id = Buffer.from(JSON.stringify(state.creds)).toString("base64");
-            // Hapa bot itatuma Session ID kwenye WhatsApp yako
             await sock.sendMessage(sock.user.id, { text: "ZUWA-MD-SESSION;;" + session_id });
             console.log("SESSION ID IMETUMWA!");
             process.exit(0);
